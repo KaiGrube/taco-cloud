bbad03e3a2bfea85d9e924defb6d788df8810500
KaiAtDCI <kaiatdci@web.de>
2022-06-21 12:25:02 +0200

    moved controllers to new subpackage 'web'

--- src/main/java/com/example/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/web/DesignTacoController.java
@@ -1,5 +1,7 @@
-package com.example.tacocloud;
+package com.example.tacocloud.web;
 
+import com.example.tacocloud.Ingredient;
+import com.example.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
